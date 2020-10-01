e3ebd617dbed0419b7af1c48c1e386c957a597be
Anffaloff <anffaloffmobile@gmail.com>
2020-10-01 15:51:03 +0300

    gulp.task('watch')

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const watch = require('gulp-watch');
 
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -9,3 +10,9 @@ gulp.task('browser-sync', function() {
     });
 });
 
+gulp.task('watch', function () {
+    watch('./src/**/*.html', gulp.parallel ( browserSync.reload ));
+    watch('./src/css/**/*.css', gulp.parallel ( browserSync.reload ));
+});
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
