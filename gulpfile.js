d777c181fb13c7381c5e7ec39b7cd994f86ff7fd
Anffaloff <anffaloffmobile@gmail.com>
2020-10-02 12:50:58 +0300

    start sccs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const watch = require('gulp-watch');
+const sass = require('gulp-sass');
 
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -13,6 +14,17 @@ gulp.task('browser-sync', function() {
 gulp.task('watch', function () {
     watch('./src/**/*.html', gulp.parallel ( browserSync.reload ));
     watch('./src/css/**/*.css', gulp.parallel ( browserSync.reload ));
+    // watch('./src/scss/**/*.scss', gulp.parallel ( browserSync.reload ));
+
+});
+
+// gulp.task('default', gulp.series('scss', gulp.parallel('browser-sync', 'watch')));
+
+gulp.task('scss', function(callback) {
+    return gulp.src ('./src/scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css/'));
+    callback();
 });
 
 gulp.task('default', gulp.parallel('browser-sync', 'watch'));
